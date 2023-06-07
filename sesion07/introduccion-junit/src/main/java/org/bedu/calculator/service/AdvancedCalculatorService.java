b7772ee2cded17386d29ece7c86e0e19017e48d3
Jorge Ramón <jorge.ramon2128@gmail.com>
2023-06-06 20:50:14 -0600

    Agregando ejercicio de factorial

---
+++ sesion07/introduccion-junit/src/main/java/org/bedu/calculator/service/AdvancedCalculatorService.java
@@ -0,0 +1,14 @@
+package org.bedu.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdvancedCalculatorService {
+
+  // Para todo número "n" >= 0
+  // n! = n * (n-1) * (n-2) * (n-3) * ... * 1
+  // 5! = 5 * 4 * 3 * 2 * 1
+  public int factorial(int n) {
+    return 0;
+  }
+}
---
+++ sesion07/introduccion-junit/src/test/java/org/bedu/calculator/service/AdvancedCalculatorServiceTest.java
@@ -0,0 +1,12 @@
+package org.bedu.calculator.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AdvancedCalculatorServiceTest {
+
+  @InjectMocks
+  private AdvancedCalculatorService service;
+}
